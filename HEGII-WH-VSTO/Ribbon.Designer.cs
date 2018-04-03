8619439e7fe6adc032a846065997511cbf6f2fc5
Li Sheng <dick.li.sheng@hotmail.com>
2018-04-03 17:22:47 +0800

    添加项目文件。

---
+++ HEGII-WH-VSTO/Ribbon.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace HEGII_WH_VSTO
+{
+    partial class Ribbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.tab2 = this.Factory.CreateRibbonTab();
+            this.group2 = this.Factory.CreateRibbonGroup();
+            this.buttonGroup1 = this.Factory.CreateRibbonButtonGroup();
+            this.ButtonInstallOrderArrange = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.tab2.SuspendLayout();
+            this.group2.SuspendLayout();
+            this.buttonGroup1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Label = "TabAddIns";
+            this.tab1.Name = "tab1";
+            // 
+            // group1
+            // 
+            this.group1.Label = "group1";
+            this.group1.Name = "group1";
+            // 
+            // tab2
+            // 
+            this.tab2.Groups.Add(this.group2);
+            this.tab2.Label = "恒洁客服";
+            this.tab2.Name = "tab2";
+            // 
+            // group2
+            // 
+            this.group2.Items.Add(this.buttonGroup1);
+            this.group2.Name = "group2";
+            // 
+            // buttonGroup1
+            // 
+            this.buttonGroup1.Items.Add(this.ButtonInstallOrderArrange);
+            this.buttonGroup1.Name = "buttonGroup1";
+            // 
+            // ButtonInstallOrderArrange
+            // 
+            this.ButtonInstallOrderArrange.Label = "安装单整理";
+            this.ButtonInstallOrderArrange.Name = "ButtonInstallOrderArrange";
+            this.ButtonInstallOrderArrange.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ButtonInstallOrderArrange_Click);
+            // 
+            // Ribbon
+            // 
+            this.Name = "Ribbon";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Tabs.Add(this.tab2);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.tab2.ResumeLayout(false);
+            this.tab2.PerformLayout();
+            this.group2.ResumeLayout(false);
+            this.group2.PerformLayout();
+            this.buttonGroup1.ResumeLayout(false);
+            this.buttonGroup1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        private Microsoft.Office.Tools.Ribbon.RibbonTab tab2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButtonGroup buttonGroup1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton ButtonInstallOrderArrange;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon Ribbon
+        {
+            get { return this.GetRibbon<Ribbon>(); }
+        }
+    }
+}
---
+++ HEGII-WH-VSTO/Ribbon.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Tools.Ribbon;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace HEGII_WH_VSTO
+{
+    public partial class Ribbon
+    {
+        private void Ribbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private bool CheckFileFormat()
+        {
+            string StringRangeRow;
+
+            Worksheet ActiveSheet = Globals.ThisAddIn.Application.ActiveSheet;
+            
+            if (ActiveSheet.Range["A:A"].Find("日期") == null)
+            {
+                return (false);
+            }
+            else
+            {
+                StringRangeRow = ActiveSheet.Range["A:A"].Find("日期").Row + ":" + ActiveSheet.Range["A:A"].Find("日期").Row;
+            }
+
+            if (ActiveSheet.Range[StringRangeRow].Find("日期") == null)
+            {
+                return (false);
+            }
+            if (ActiveSheet.Range[StringRangeRow].Find("单号") == null)
+            {
+                return (false);
+            }
+            if (ActiveSheet.Range[StringRangeRow].Find("部门") == null)
+            {
+                return (false);
+            }
+            if (ActiveSheet.Range[StringRangeRow].Find("联系人") == null)
+            {
+                return (false);
+            }
+            if (ActiveSheet.Range[StringRangeRow].Find("联系电话") == null)
+            {
+                return (false);
+            }
+            if (ActiveSheet.Range[StringRangeRow].Find("送货地点") == null)
+            {
+                return (false);
+            }
+            if (ActiveSheet.Range[StringRangeRow].Find("安装备注") == null)
+            {
+                return (false);
+            }
+            if (ActiveSheet.Range[StringRangeRow].Find("商品名称") == null)
+            {
+                return (false);
+            }
+            if (ActiveSheet.Range[StringRangeRow].Find("安装数量") == null)
+            {
+                return (false);
+            }
+            return (true);
+        }
+
+        private void ButtonInstallOrderArrange_Click(object sender, RibbonControlEventArgs e)
+        {
+            if (CheckFileFormat())
+            {
+                Excel.Application NewEXCELFile = new Excel.Application();
+                Workbook NewWorkbook = NewEXCELFile.Application.Workbooks.Add();
+                Worksheet NewWorksheet = NewWorkbook.Worksheets.Add();
+                InstallOrderArrange(NewWorkbook, NewWorksheet);
+
+
+
+                NewEXCELFile.Visible = true;
+            }
+            else
+            {
+                MessageBox.Show("文件格式不正确，请重新核对数据！");
+            }
+        }
+
+        private void InstallOrderArrange (Workbook NewWorkbook, Worksheet NewWorksheet)
+        {
+            NewWorksheet.Cells[1, 2] = "装/修";
+            NewWorksheet.Cells[1, 3] = "销售点";
+            NewWorksheet.Cells[1, 4] = "报装日期";
+            NewWorksheet.Cells[1, 5] = "预约日期";
+            NewWorksheet.Cells[1, 6] = "用户名称";
+            NewWorksheet.Cells[1, 7] = "大范围";
+            NewWorksheet.Cells[1, 8] = "销售人员";
+            NewWorksheet.Cells[1, 9] = "地址";
+            NewWorksheet.Cells[1, 10] = "电话";
+            NewWorksheet.Cells[1, 11] = "安装产品";
+
+            Worksheet ActiveSheet = Globals.ThisAddIn.Application.ActiveSheet;
+
+            int i = 7, j = 2;
+            while (ActiveSheet.Cells[i,1] != null)
+            {
+
+            }
+
+        }
+
+
+    }
+}
